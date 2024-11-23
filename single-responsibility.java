3a864a7c4fc963f2694f5f57898b2ae6de2686b7
navin <95286412+sahnavin123@users.noreply.github.com>
2024-11-23 15:40:51 +0530

    crated files for system design

---
+++ single-responsibility.java
@@ -0,0 +1,20 @@
+// Marker Entity
+
+class Marker {
+    String name; 
+    String color; 
+    int year ; 
+    int price; 
+
+
+    public Marker (String name, String color, int year, int price){
+        this.name = name; 
+        this.color = color; 
+        this.year = year ;
+        this.price = price;
+    }
+
+    class Invoice {
+        
+    }
+}
